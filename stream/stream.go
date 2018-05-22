76805067ddb21b2ab882aa6e0467b20bd7926fdb
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-05-22 15:42:00 +0530

    [sudhanshu] broke stream processing into functions, removed points with pace faster than world record pace

--- stream/stream.go
+++ stream/stream.go
@@ -15,6 +15,56 @@ type Stream struct {
 	Drops []Drop
 }
 
+// Load a stream from a file
+func (s *Stream) Load(filePath string) error {
+
+	g, err := gpx.ParseFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	for _, track := range g.Tracks {
+		for _, trackSegment := range track.TrackSegments {
+			for _, trackPoint := range trackSegment.TrackPoint {
+				s.AddDrop(trackPoint)
+			}
+		}
+	}
+
+	s.removeInvalidData()
+
+	return nil
+}
+
+// AddDrop to the stream
+func (s *Stream) AddDrop(point gpx.TrackPoint) {
+
+	var previous Drop
+	if len(s.Drops) > 0 {
+		previous = s.Drops[len(s.Drops)-1]
+	}
+
+	d := Drop{}
+	d.DataPoints = previous.DataPoints + 1
+
+	d.addTime(&point, &previous)
+	d.addDistance(&point, &previous)
+	d.addPace(&point, &previous)
+	d.addTemperature(&point, &previous)
+	d.addHeartRate(&point, &previous)
+	d.addCadence(&point, &previous)
+
+	valid := d.checkValidity(&point, &previous)
+
+	if valid {
+		s.Drops = append(s.Drops, d)
+	}
+}
+
+func (s *Stream) removeInvalidData() {
+
+}
+
 // Drop is an individual point of data
 type Drop struct {
 	DataPoints  int
@@ -48,69 +98,51 @@ func (d *Drop) Display() {
 	fmt.Printf("[%v][%v] %v,%v ^%v s%v c%v h%v d%v %v (%v) <%v> [A] ^%v +%v %v (%v) [M] c%v h%v d%v\n", d.Timestamp, d.DataPoints, d.Latitude, d.Longitude, int(d.Elevation), d.Seconds, d.Temperature, d.HeartRate, d.Cadence, d.Distance, d.Distance3D, d.Pace, int(d.Aggregates.Climb), d.Aggregates.Time, int(d.Aggregates.Distance), int(d.Aggregates.Distance3D), d.Maximums.Temperature, d.Maximums.HeartRate, d.Maximums.Cadence)
 }
 
-// Load a stream from a file
-func (s *Stream) Load(filePath string) error {
-
-	g, err := gpx.ParseFile(filePath)
-	if err != nil {
-		return err
-	}
-
-	for _, track := range g.Tracks {
-		for _, trackSegment := range track.TrackSegments {
-			for _, trackPoint := range trackSegment.TrackPoint {
-				s.AddDrop(trackPoint)
-			}
-		}
-	}
-
-	return nil
-}
-
-// AddDrop to the stream
-func (s *Stream) AddDrop(point gpx.TrackPoint) {
-
-	var previous Drop
-	if len(s.Drops) > 0 {
-		previous = s.Drops[len(s.Drops)-1]
-	}
-
-	d := Drop{}
-
+func (d *Drop) addTime(point *gpx.TrackPoint, previous *Drop) {
 	t, err := time.Parse(time.RFC3339, point.Timestamp)
 	if err != nil {
 		log.Fatalf("Could not parse %v", point.Timestamp)
 	}
 	d.Timestamp = t
 
+	if d.DataPoints > 1 {
+		d.Seconds = d.Timestamp.Sub(previous.Timestamp).Seconds()
+		d.Aggregates.Time = previous.Aggregates.Time + d.Seconds
+	}
+}
+
+func (d *Drop) addDistance(point *gpx.TrackPoint, previous *Drop) {
 	if point.Latitude != 0.0 && point.Longitude != 0.0 {
-		d.DataPoints = previous.DataPoints + 1
+
 		d.Latitude = float64(point.Latitude)
 		d.Longitude = float64(point.Longitude)
 		d.Elevation = float64(point.Elevation)
 
-		if ((d.Elevation - previous.Elevation) > 0) && (d.DataPoints != 1) {
+		if ((d.Elevation - previous.Elevation) > 0) && (d.DataPoints > 1) {
 			d.Aggregates.Climb = previous.Aggregates.Climb + (d.Elevation - previous.Elevation)
 		} else {
 			d.Aggregates.Climb = previous.Aggregates.Climb
 		}
 
-		if len(s.Drops) > 0 {
+		if d.DataPoints > 1 {
 			d.Distance = geo.DistanceInMetresBetweenPoints(geo.Point{Latitude: d.Latitude, Longitude: d.Longitude, Elevation: d.Elevation}, geo.Point{Latitude: previous.Latitude, Longitude: previous.Longitude, Elevation: previous.Elevation})
 			d.Distance3D = geo.DistanceInMetresIncludingElevation(geo.Point{Latitude: d.Latitude, Longitude: d.Longitude, Elevation: d.Elevation}, geo.Point{Latitude: previous.Latitude, Longitude: previous.Longitude, Elevation: previous.Elevation})
-
-			d.Pace = math.Round((1000/(d.Distance3D*60))*10000) / 10000
 
 			d.Aggregates.Distance = previous.Aggregates.Distance + d.Distance
 			d.Aggregates.Distance3D = previous.Aggregates.Distance3D + d.Distance3D
 		}
+	}
+}
 
-		if d.DataPoints != 1 {
-			d.Seconds = d.Timestamp.Sub(previous.Timestamp).Seconds()
-			d.Aggregates.Time = previous.Aggregates.Time + d.Seconds
+func (d *Drop) addPace(point *gpx.TrackPoint, previous *Drop) {
+	if point.Latitude != 0.0 && point.Longitude != 0.0 {
+		if d.DataPoints > 1 {
+			d.Pace = math.Round(((1000*d.Seconds)/(d.Distance3D*60))*10000) / 10000
 		}
 	}
+}
 
+func (d *Drop) addTemperature(point *gpx.TrackPoint, previous *Drop) {
 	if point.Extensions.TrackPointExtensions.Temperature != 0 {
 		d.Temperature = int(point.Extensions.TrackPointExtensions.Temperature)
 
@@ -120,7 +152,9 @@ func (s *Stream) AddDrop(point gpx.TrackPoint) {
 			d.Maximums.Temperature = previous.Maximums.Temperature
 		}
 	}
+}
 
+func (d *Drop) addHeartRate(point *gpx.TrackPoint, previous *Drop) {
 	if point.Extensions.TrackPointExtensions.HeartRate != 0 {
 		d.HeartRate = int(point.Extensions.TrackPointExtensions.HeartRate)
 
@@ -130,7 +164,9 @@ func (s *Stream) AddDrop(point gpx.TrackPoint) {
 			d.Maximums.HeartRate = previous.Maximums.HeartRate
 		}
 	}
+}
 
+func (d *Drop) addCadence(point *gpx.TrackPoint, previous *Drop) {
 	if point.Extensions.TrackPointExtensions.Cadence != 0 {
 		d.Cadence = int(point.Extensions.TrackPointExtensions.Cadence)
 
@@ -140,6 +176,12 @@ func (s *Stream) AddDrop(point gpx.TrackPoint) {
 			d.Maximums.Cadence = previous.Maximums.Cadence
 		}
 	}
+}
 
-	s.Drops = append(s.Drops, d)
+func (d *Drop) checkValidity(point *gpx.TrackPoint, previous *Drop) bool {
+	// If pace is under 2.5 minutes/km, it's faster than world record timing for 5k
+	if d.DataPoints > 1 && d.Pace < 2.5 {
+		return false
+	}
+	return true
 }
--- stream/stream_test.go
+++ stream/stream_test.go
@@ -10,14 +10,23 @@ import (
 )
 
 func Test_LoadStream(t *testing.T) {
-	s := stream.Stream{Drops: []stream.Drop{}}
-	// s.Load("../external/strava-8kDelhi-May18.gpx")
-	// s.Load("../external/strava-BadGPS-Singapore.gpx")
-	// s.Load("../external/strava-Mumbai-Jan15.gpx")
-	s.Load("../external/strava-ADHM-2014.gpx")
+	adhm := stream.Stream{Drops: []stream.Drop{}}
+	adhm.Load("../external/strava-ADHM-2014.gpx")
 
-	final := s.Drops[len(s.Drops)-1]
-	assert.Equal(t, 21422.215, math.Round(final.Aggregates.Distance*1000)/1000)
-	assert.Equal(t, 21440.655, math.Round(final.Aggregates.Distance3D*1000)/1000)
+	final := adhm.Drops[len(adhm.Drops)-1]
+	assert.Equal(t, 21422.214, math.Round(final.Aggregates.Distance*1000)/1000)
+	assert.Equal(t, 21440.654, math.Round(final.Aggregates.Distance3D*1000)/1000)
 	assert.Equal(t, 7520, int(final.Aggregates.Time))
+
+	bad := stream.Stream{Drops: []stream.Drop{}}
+	bad.Load("../external/strava-BadGPS-Singapore.gpx")
+
+	final = bad.Drops[len(bad.Drops)-1]
+	assert.Equal(t, 8657.374, math.Round(final.Aggregates.Distance*1000)/1000)
+	assert.Equal(t, 8674.776, math.Round(final.Aggregates.Distance3D*1000)/1000)
+	assert.Equal(t, 2304, int(final.Aggregates.Time))
+
+	// s.Load("../external/strava-8kDelhi-May18.gpx")
+	// s.Load("../external/strava-Mumbai-Jan15.gpx")
+
 }
